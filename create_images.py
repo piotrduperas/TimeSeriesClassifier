9eec6d25a97f8d1f26b19ca4ea81a8bd0ee64f7b
Marcin Galiński <marcinggalinski@gmail.com>
2021-10-21 19:16:39 +0200

    make code somewhat understandable

--- create_images.py
+++ create_images.py
@@ -3,6 +3,7 @@ import imageio
 import keras
 import numpy
 import os
+import random
 
 from keras.layers import Conv2D, Dense, Dropout, Flatten, MaxPooling2D
 from keras.models import Sequential
@@ -12,9 +13,6 @@ from pandas import read_csv
 from PIL import Image, ImageDraw
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras import optimizers
-
-# from matplotlib import pyplot
-# from mpl_toolkits.mplot3d import Axes3D
 
 
 IMAGE_WIDTH = 64
@@ -36,79 +34,74 @@ for (directory, category, file) in files:
 #    pass
 #for i in range(1,1):
     dataframe = read_csv(f"{path.join(directory, category, file)}.csv", header=None)
-    array = dataframe.values
-    # separate array into input and output components
-    X = array[:]
-    diff = numpy.diff(X, axis=0)
-    aaa = numpy.vstack((diff, numpy.array([[0, 0, 0]])))
+    X = dataframe.values
 
-    X = numpy.hstack((X, aaa))
+    # add vector of 1st differentials
+    differentials = numpy.diff(X, axis=0)
+    replenished_differentials = numpy.vstack([numpy.array([[0, 0, 0]]), differentials])
+    X = numpy.hstack([X, replenished_differentials])
 
-    aaa = numpy.vstack((numpy.diff(diff, axis=0), numpy.array([[0, 0, 0], [0, 0, 0]])))
+    # add vector of 2nd differentials
+    second_differentials = numpy.diff(differentials, axis=0)
+    replenished_second_differentials = numpy.vstack([numpy.array([[0, 0, 0], [0, 0, 0]]), second_differentials])
+    X = numpy.hstack([X, replenished_second_differentials])
 
-    X = numpy.hstack((X, aaa))
-
-    #X[:-1,:-1] = X[1:, 0:3] - X[:-1, 0:3]
     scaler = MinMaxScaler(feature_range=(0, 1))
     xmin = X.min()
     xmax = X.max()
     scaler.fit(numpy.array([
+        # x     y     z      dx         dy         dz         ddx        ddy        ddz
         [xmin, xmin, xmin, xmin / 14, xmin / 14, xmin / 14, xmin / 14, xmin / 14, xmin / 14],
         [xmax, xmax, xmax, xmax / 14, xmax / 14, xmax / 14, xmax / 14, xmax / 14, xmax / 14]
     ]))
-    rX = scaler.transform(X)
-    # summarize transformed data
-    numpy.set_printoptions(precision=3)
+    scaled_X = scaler.transform(X)
 
-    for dim in range(0, 1):
-        out = Image.new("RGB", (IMAGE_WIDTH, IMAGE_HEIGHT), (0, 0, 0))
+    out = Image.new("RGB", (IMAGE_WIDTH, IMAGE_HEIGHT), (0, 0, 0))
 
-        # get a drawing context
-        d = ImageDraw.Draw(out)
+    # get a drawing context
+    d = ImageDraw.Draw(out)
 
-        for j in range(1, len(rX)):
-            row = rX[j]
-            m = j
-            x, y = (row[dim] * IMAGE_HEIGHT % IMAGE_HEIGHT), (row[(dim + 6)] * IMAGE_HEIGHT) % IMAGE_HEIGHT
-            uu = out.getpixel((x, y))
-            d.point((x, y), fill=(255 - m * 5 // 6, uu[1], uu[2]))
-            uu = out.getpixel(((row[dim + 1] * IMAGE_HEIGHT % IMAGE_HEIGHT), (row[(dim + 7)] * IMAGE_HEIGHT) % IMAGE_HEIGHT))
+    for i in range(0, len(scaled_X)):
+        row = scaled_X[i]
 
-            d.point((row[dim + 1] * IMAGE_HEIGHT, row[(dim + 7)] * IMAGE_HEIGHT), fill=(uu[0], 255 - m * 5 // 6, uu[2]))
-            uu = out.getpixel(((row[dim + 2] * IMAGE_HEIGHT % IMAGE_HEIGHT), (row[(dim + 8)] * IMAGE_HEIGHT) % IMAGE_HEIGHT))
+        # x and ddx
+        x, y = (row[0] * IMAGE_HEIGHT % IMAGE_HEIGHT), (row[(6)] * IMAGE_HEIGHT) % IMAGE_HEIGHT
 
-            d.point((row[dim + 2] * IMAGE_HEIGHT, row[(dim + 8)] * IMAGE_HEIGHT), fill=(uu[0], uu[1], 255 - m * 5 // 6))
+        # red
+        current_color = out.getpixel((x, y))
+        d.point((x, y), fill=(255 - i * 5 // 6, current_color[1], current_color[2]))
 
-        # draw multiline text
-        #out.show()
-        out.save(f"pictures/{category}_{file}_{dim}.png", "PNG")
+        # green
+        current_color = out.getpixel(((row[1] * IMAGE_HEIGHT % IMAGE_HEIGHT), (row[7] * IMAGE_HEIGHT) % IMAGE_HEIGHT))
+        d.point((row[1] * IMAGE_HEIGHT, row[7] * IMAGE_HEIGHT),
+                fill=(current_color[0], 255 - i * 5 // 6, current_color[2]))
 
-    #fig = pyplot.figure()
-    #ax = Axes3D(fig)
+        # blue
+        current_color = out.getpixel(((row[2] * IMAGE_HEIGHT % IMAGE_HEIGHT), (row[8] * IMAGE_HEIGHT) % IMAGE_HEIGHT))
+        d.point((row[0 + 2] * IMAGE_HEIGHT, row[(0 + 8)] * IMAGE_HEIGHT),
+                fill=(current_color[0], current_color[1], 255 - i * 5 // 6))
 
-    #sequence_containing_x_vals = rX[:,0]
-    #sequence_containing_y_vals = rX[:,1]
-    #sequence_containing_z_vals = rX[:,2]
-
-    #ax.scatter(sequence_containing_x_vals, sequence_containing_y_vals, sequence_containing_z_vals)
-    #pyplot.show()
+    out.save(f"pictures/{category}_{file}.png", "PNG")
 
 
 im_x = []
 im_y = []
 
-for im_path in glob.glob("pictures/*.png"):
-    im = imageio.imread(im_path)
-    img_name = path.split(im_path)[-1].split('.')[0]
-    img_dim = img_name.split('_')[2]
-    img_cat = img_name.split('_')[0]
-    if img_dim == '0':
-        im_x.append(im[:, :])
-        im_y.append(int(img_cat) - 1)
-    # do whatever wit
+
+image_paths = glob.glob("pictures/*.png")
+random.shuffle(image_paths)
+
+for image_path in image_paths:
+    im = imageio.imread(image_path)
+    image_name = path.split(image_path)[-1].split('.')[0]
+    image_category = image_name.split('_')[0]
+    im_x.append(im[:, :])
+    im_y.append(int(image_category) - 1)
+
 
 im_x = numpy.array(im_x)
-im_x = im_x.reshape((im_x.shape[0], IMAGE_WIDTH, IMAGE_HEIGHT, 3))
+im_x = im_x.reshape([im_x.shape[0], IMAGE_WIDTH, IMAGE_HEIGHT, 3])
+
 
 x_train = im_x[:2500].astype("float32")
 x_test = im_x[2500:].astype("float32")
@@ -116,6 +109,7 @@ x_train /= 255
 x_test /= 255
 
 num_classes = 8
+
 y_train = keras.utils.np_utils.to_categorical(im_y[:2500], num_classes)
 y_test = keras.utils.np_utils.to_categorical(im_y[2500:], num_classes)
 

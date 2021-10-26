51ab716993f8418a4faa9dd2cf4e0509dea94d0f
Piotr Duperas <piotrduperas@gmail.com>
2021-10-26 21:32:35 +0200

    Some improvements

--- classify.py
+++ classify.py
@@ -5,7 +5,7 @@ import numpy
 import os
 import random
 
-from keras.layers import Conv2D, Dense, Dropout, Flatten, MaxPooling2D
+from keras.layers import Conv2D, Dense, Dropout, Flatten, MaxPooling2D, AveragePooling2D
 from keras.models import Sequential
 from keras.utils import np_utils
 from os import path
@@ -13,7 +13,6 @@ from pandas import read_csv
 from PIL import Image, ImageDraw
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras import optimizers
-
 
 def add_differentials(data: numpy.ndarray, differential_count: int) -> numpy.ndarray:
     result = data.copy()
@@ -29,13 +28,13 @@ def add_differentials(data: numpy.ndarray, differential_count: int) -> numpy.nda
 
 
 # constants
-IMAGE_WIDTH = 64
-IMAGE_HEIGHT = 64
+IMAGE_WIDTH = 32
+IMAGE_HEIGHT = 32
 CATEGORY_COUNT = 8
 
 directories = ["data/test", "data/train"]
 
-files: list[tuple[str, str, str]] = []
+files = []
 
 for directory in directories:
     for category in range(1, CATEGORY_COUNT + 1):
@@ -45,16 +44,23 @@ for directory in directories:
 
 for (directory, category, file) in files:
     dataframe = read_csv(f"{path.join(directory, category, file)}.csv", header=None)
+    X_raw = dataframe.values
     X = dataframe.values
+
+    for i in range(2, len(X) - 2):
+      X[i] = (X[i] * 4 + X[i-1] * 2 + X[i+1] * 2 + X[i-2] + X[i+2]) / 10;
+
     X = add_differentials(X, 2)
+
+    v = 12
 
     scaler = MinMaxScaler(feature_range=(0, 1))
     xmin = X.min()
     xmax = X.max()
     scaler.fit(numpy.array([
         # x     y     z      dx         dy         dz         ddx        ddy        ddz
-        [xmin, xmin, xmin, xmin / 14, xmin / 14, xmin / 14, xmin / 14, xmin / 14, xmin / 14],
-        [xmax, xmax, xmax, xmax / 14, xmax / 14, xmax / 14, xmax / 14, xmax / 14, xmax / 14]
+        [xmin, xmin, xmin, xmin / v, xmin / v, xmin / v, xmin / v, xmin / v, xmin / v],
+        [xmax, xmax, xmax, xmax / v, xmax / v, xmax / v, xmax / v, xmax / v, xmax / v]
     ]))
     scaled_X = scaler.transform(X)
 
@@ -66,22 +72,28 @@ for (directory, category, file) in files:
     for i in range(0, len(scaled_X)):
         row = scaled_X[i]
 
+        intensity = 255 - i * 5 // 6
+        #if intensity < 5:
+        #  intensity = 5
+        x_row = 0
+        y_row = 3
+
         # x and ddx
-        x, y = (row[0] * IMAGE_HEIGHT % IMAGE_HEIGHT), (row[6] * IMAGE_HEIGHT) % IMAGE_HEIGHT
+        x, y = (row[x_row] * IMAGE_HEIGHT % IMAGE_HEIGHT), (row[y_row] * IMAGE_HEIGHT) % IMAGE_HEIGHT
 
         # red
         current_color = out.getpixel((x, y))
-        d.point((x, y), fill=(255 - i * 5 // 6, current_color[1], current_color[2]))
+        d.point((x, y), fill=(intensity, current_color[1], current_color[2]))
 
         # green
-        current_color = out.getpixel(((row[1] * IMAGE_HEIGHT % IMAGE_HEIGHT), (row[7] * IMAGE_HEIGHT) % IMAGE_HEIGHT))
-        d.point((row[1] * IMAGE_HEIGHT, row[7] * IMAGE_HEIGHT),
-                fill=(current_color[0], 255 - i * 5 // 6, current_color[2]))
+        current_color = out.getpixel((((row[x_row + 1] * IMAGE_HEIGHT) % IMAGE_HEIGHT), ((row[y_row + 1] * IMAGE_HEIGHT)) % IMAGE_HEIGHT))
+        d.point((row[x_row + 1] * IMAGE_HEIGHT, row[y_row + 1] * IMAGE_HEIGHT),
+                fill=(current_color[0], intensity, current_color[2]))
 
         # blue
-        current_color = out.getpixel(((row[2] * IMAGE_HEIGHT % IMAGE_HEIGHT), (row[8] * IMAGE_HEIGHT) % IMAGE_HEIGHT))
-        d.point((row[0 + 2] * IMAGE_HEIGHT, row[(0 + 8)] * IMAGE_HEIGHT),
-                fill=(current_color[0], current_color[1], 255 - i * 5 // 6))
+        current_color = out.getpixel(((row[x_row + 2] * IMAGE_HEIGHT % IMAGE_HEIGHT), (row[y_row + 2] * IMAGE_HEIGHT) % IMAGE_HEIGHT))
+        d.point((row[x_row + 2] * IMAGE_HEIGHT, row[(y_row + 2)] * IMAGE_HEIGHT),
+                fill=(current_color[0], current_color[1], intensity))
 
     out.save(f"pictures/{category}_{file}.png", "PNG")
 
@@ -120,9 +132,13 @@ model.add(Conv2D(64, kernel_size=(3, 3),
                  activation="relu",
                  input_shape=(IMAGE_WIDTH, IMAGE_HEIGHT, 3)))
 model.add(Conv2D(128, (4, 4), activation="relu"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(MaxPooling2D(pool_size=(4, 4)))
 model.add(Dropout(0.25))
 model.add(Flatten())
+model.add(Dense(128, activation="relu"))
+model.add(Dropout(0.25))
+model.add(Dense(128, activation="relu"))
+model.add(Dropout(0.25))
 model.add(Dense(128, activation="relu"))
 model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation="softmax"))
@@ -132,11 +148,7 @@ model.compile(loss=keras.losses.categorical_crossentropy,
               metrics=["accuracy"])
 
 model.fit(x_train, y_train,
-          batch_size=128,
-          epochs=8,
+          batch_size=64,
+          epochs=24,
           verbose=1,
           validation_data=(x_test, y_test))
-
-score = model.evaluate(x_test, y_test, verbose=0)
-print("Test loss:", score[0])
-print("Test accuracy:", score[1])

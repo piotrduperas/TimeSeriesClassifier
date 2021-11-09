2d59ed704fbf2668705e3b9c33c456b60d955261
Piotr Duperas <piotrduperas@gmail.com>
2021-11-09 18:35:13 +0100

    95% accuracy

--- classify.py
+++ classify.py
@@ -13,7 +13,6 @@ from pandas import read_csv
 from PIL import Image, ImageDraw
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras import optimizers
-
 def add_differentials(data: numpy.ndarray, differential_count: int) -> numpy.ndarray:
     result = data.copy()
     filler = numpy.zeros(3)
@@ -28,8 +27,8 @@ def add_differentials(data: numpy.ndarray, differential_count: int) -> numpy.nda
 
 
 # constants
-IMAGE_WIDTH = 32
-IMAGE_HEIGHT = 32
+IMAGE_WIDTH = 28
+IMAGE_HEIGHT = 28
 CATEGORY_COUNT = 8
 
 directories = ["data/test", "data/train"]
@@ -47,20 +46,23 @@ for (directory, category, file) in files:
     X_raw = dataframe.values
     X = dataframe.values
 
+
+
+    X = add_differentials(X, 3)
+    X_raw = add_differentials(X_raw, 3)
+
     for i in range(2, len(X) - 2):
-      X[i] = (X[i] * 4 + X[i-1] * 2 + X[i+1] * 2 + X[i-2] + X[i+2]) / 10;
+        X[i] = (X_raw[i] * 2 + X_raw[i-1] * 1 + X_raw[i+1] * 1) / 4;
 
-    X = add_differentials(X, 2)
-
-    v = 12
+    v = 14
 
     scaler = MinMaxScaler(feature_range=(0, 1))
     xmin = X.min()
     xmax = X.max()
     scaler.fit(numpy.array([
         # x     y     z      dx         dy         dz         ddx        ddy        ddz
-        [xmin, xmin, xmin, xmin / v, xmin / v, xmin / v, xmin / v, xmin / v, xmin / v],
-        [xmax, xmax, xmax, xmax / v, xmax / v, xmax / v, xmax / v, xmax / v, xmax / v]
+        [xmin, xmin, xmin, xmin / v, xmin / v, xmin / v, xmin / v / 3, xmin / v / 3, xmin / v / 3, xmin / v / 3, xmin / v / 3, xmin / v / 3],
+        [xmax, xmax, xmax, xmax / v, xmax / v, xmax / v, xmax / v / 3, xmax / v / 3, xmax / v / 3, xmax / v / 3, xmax / v / 3, xmax / v / 3]
     ]))
     scaled_X = scaler.transform(X)
 
@@ -69,14 +71,74 @@ for (directory, category, file) in files:
     # get a drawing context
     d = ImageDraw.Draw(out)
 
-    for i in range(0, len(scaled_X)):
+    mid = IMAGE_HEIGHT // 2 - 1
+
+        
+    for ii in range(0, len(scaled_X)):
+        i = len(scaled_X) - 1 - ii
         row = scaled_X[i]
 
-        intensity = 255 - i * 5 // 6
+        intensity = 255 - i * 8 // 9
+        intensity = intensity // 3
+        #if intensity < 5:
+        #  intensity = 5
+        x_row = 6
+        y_row = 6
+
+        # x and ddx
+        x, y = (row[x_row] * IMAGE_HEIGHT % IMAGE_HEIGHT), mid
+
+        # red
+        current_color = out.getpixel((x, mid))
+        d.point((x, mid), fill=(intensity, current_color[1], current_color[2]))
+
+        # green
+        current_color = out.getpixel((((row[x_row + 1] * IMAGE_HEIGHT) % IMAGE_HEIGHT), mid))
+        d.point((row[x_row + 1] * IMAGE_HEIGHT, mid),
+                fill=(current_color[0], intensity, current_color[2]))
+
+        # blue
+        current_color = out.getpixel(((row[x_row + 2] * IMAGE_HEIGHT % IMAGE_HEIGHT), mid))
+        d.point((row[x_row + 2] * IMAGE_HEIGHT, mid),
+                fill=(current_color[0], current_color[1], intensity))
+
+        
+    for ii in range(0, len(scaled_X)):
+        i = len(scaled_X) - 1 - ii
+        row = scaled_X[i]
+
+        intensity = 255 - i * 8 // 9
+        #if intensity < 5:
+        #  intensity = 5
+        x_row = 3
+        y_row = 3
+
+        # x and ddx
+        x, y = (row[x_row] * IMAGE_HEIGHT % IMAGE_HEIGHT), (row[y_row] * IMAGE_HEIGHT) % IMAGE_HEIGHT
+
+        # red
+        current_color = out.getpixel((x, IMAGE_HEIGHT - 1 - y))
+        d.point((x, IMAGE_HEIGHT - 1 - y), fill=(intensity, current_color[1], current_color[2]))
+
+        # green
+        current_color = out.getpixel((((row[x_row + 1] * IMAGE_HEIGHT) % IMAGE_HEIGHT), IMAGE_HEIGHT - 1 - ((row[y_row + 1] * IMAGE_HEIGHT)) % IMAGE_HEIGHT))
+        d.point((row[x_row + 1] * IMAGE_HEIGHT, IMAGE_HEIGHT - 1 - row[y_row + 1] * IMAGE_HEIGHT),
+                fill=(current_color[0], intensity, current_color[2]))
+
+        # blue
+        current_color = out.getpixel(((row[x_row + 2] * IMAGE_HEIGHT % IMAGE_HEIGHT), IMAGE_HEIGHT - 1 - (row[y_row + 2] * IMAGE_HEIGHT) % IMAGE_HEIGHT))
+        d.point((row[x_row + 2] * IMAGE_HEIGHT, IMAGE_HEIGHT - 1 - row[(y_row + 2)] * IMAGE_HEIGHT),
+                fill=(current_color[0], current_color[1], intensity))
+        
+    for ii in range(0, len(scaled_X)):
+        i = len(scaled_X) - 1 - ii
+        row = scaled_X[i]
+
+        intensity = 255 - i * 8 // 9
         #if intensity < 5:
         #  intensity = 5
         x_row = 0
-        y_row = 3
+        y_row = 0
 
         # x and ddx
         x, y = (row[x_row] * IMAGE_HEIGHT % IMAGE_HEIGHT), (row[y_row] * IMAGE_HEIGHT) % IMAGE_HEIGHT
@@ -94,6 +156,7 @@ for (directory, category, file) in files:
         current_color = out.getpixel(((row[x_row + 2] * IMAGE_HEIGHT % IMAGE_HEIGHT), (row[y_row + 2] * IMAGE_HEIGHT) % IMAGE_HEIGHT))
         d.point((row[x_row + 2] * IMAGE_HEIGHT, row[(y_row + 2)] * IMAGE_HEIGHT),
                 fill=(current_color[0], current_color[1], intensity))
+    
 
     out.save(f"pictures/{category}_{file}.png", "PNG")
 
@@ -117,28 +180,25 @@ im_x = numpy.array(im_x)
 im_x = im_x.reshape([im_x.shape[0], IMAGE_WIDTH, IMAGE_HEIGHT, 3])
 
 
-x_train = im_x[:2500].astype("float32")
-x_test = im_x[2500:].astype("float32")
+x_train = im_x[:2600].astype("float32")
+x_test = im_x[2600:].astype("float32")
 x_train /= 255
 x_test /= 255
 
 num_classes = 8
 
-y_train = keras.utils.np_utils.to_categorical(im_y[:2500], num_classes)
-y_test = keras.utils.np_utils.to_categorical(im_y[2500:], num_classes)
+y_train = keras.utils.np_utils.to_categorical(im_y[:2600], num_classes)
+y_test = keras.utils.np_utils.to_categorical(im_y[2600:], num_classes)
 
 model = Sequential()
 model.add(Conv2D(64, kernel_size=(3, 3),
                  activation="relu",
                  input_shape=(IMAGE_WIDTH, IMAGE_HEIGHT, 3)))
-model.add(Conv2D(128, (4, 4), activation="relu"))
-model.add(MaxPooling2D(pool_size=(4, 4)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, (3, 3), activation="relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 model.add(Flatten())
-model.add(Dense(128, activation="relu"))
-model.add(Dropout(0.25))
-model.add(Dense(128, activation="relu"))
-model.add(Dropout(0.25))
 model.add(Dense(128, activation="relu"))
 model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation="softmax"))
@@ -149,6 +209,7 @@ model.compile(loss=keras.losses.categorical_crossentropy,
 
 model.fit(x_train, y_train,
           batch_size=64,
-          epochs=24,
+          epochs=64,
           verbose=1,
           validation_data=(x_test, y_test))
+          

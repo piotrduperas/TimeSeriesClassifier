ba32a151e04737be3d2ca111cdad136fee8b813c
Marcin Galiński <marcinggalinski@gmail.com>
2021-11-10 10:45:09 +0100

    bring back final results

--- classify.py
+++ classify.py
@@ -248,3 +248,7 @@ model.fit(x_train, y_train,
           epochs=64,
           verbose=1,
           validation_data=(x_test, y_test))
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print("Test loss:", score[0])
+print("Test accuracy:", score[1])

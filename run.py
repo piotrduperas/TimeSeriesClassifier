44300cf886bafc7e1c73eda1e6aba208194f2ae3
Piotr Duperas <piotrduperas@gmail.com>
2021-11-14 08:30:52 +0100

    Results

---
+++ run.py
@@ -0,0 +1,16 @@
+import subprocess
+import sys
+
+dataset = sys.argv[1]
+datasetid = dataset.lower()
+
+result = subprocess.run(['wget', 'https://www.timeseriesclassification.com/Downloads/'+dataset+'.zip', '-O', 'datasets/'+dataset+'.zip'], stdout=subprocess.PIPE)
+print(result.stdout)
+
+result = subprocess.run(['mkdir', 'datasets/'+datasetid], stdout=subprocess.PIPE)
+result = subprocess.run(['unzip', '-o', 'datasets/'+dataset+'.zip', '-d', 'datasets/'+datasetid], stdout=subprocess.PIPE)
+print(result.stdout)
+result = subprocess.run(['python3', 'convert.py', 'datasets/'+datasetid+'/'+dataset+'_TRAIN.arff', 'datasets/'+datasetid+'/'+dataset+'_TEST.arff', 'datasets/'+datasetid+'/converted'], stdout=subprocess.PIPE)
+result = subprocess.run(['python3', 'classify.py', 'datasets/'+datasetid+'/converted'], stdout=subprocess.PIPE)
+result = subprocess.run(['python3', 'classify.py', 'datasets/'+datasetid+'/converted'], stdout=subprocess.PIPE)
+print(result.stdout)
---
+++ run_everything.py
@@ -0,0 +1,23 @@
+import csv
+import subprocess
+
+with open("comparison.csv") as fp:
+    reader = csv.reader(fp, delimiter=",", quotechar='"')
+    # next(reader, None)  # skip the headers
+    data_read = [row for row in reader]
+
+for i in range(1, 6):
+    for d in data_read[1:]:
+        result = subprocess.run(['python3', 'run.py', d[0]],stdout=subprocess.PIPE)
+        # Read all from result stdout
+        try:
+            acc = str(result.stdout.decode('utf-8')).split('Test accuracy: ')[1].split('\\n')[0]
+            d.append(acc)
+            print(d)
+        except:
+            print("error")
+
+with open("results.csv", "wt") as fp:
+    writer = csv.writer(fp, delimiter=",")
+    writer.writerow(data_read[0])  # write header
+    writer.writerows(data_read[1:])
